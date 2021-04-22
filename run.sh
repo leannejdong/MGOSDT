d622acfa4fc06cb87ef0f91dd8489b4328d2d46d
Leanne Dong <jdleanne@gmail.com>
2021-04-22 11:51:13 -0400

    add ./ to gosdt, rm ./ before experiments

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 #gosdt m2.csv config.json 2>&1 | tee outputs/m2.json
 
 ./gosdt experiments/datasets/monk_1/m1.csv experiments/configurations/debug.json >outputs/m1.json
-./gosdt m2.csv ./experiments/configurations/debug.json >outputs/m2.json
-gosdt ./experiments/datasets/monk_3/m3.csv ./experiments/configurations/debug.json >outputs/m3.json
-gosdt syn3.txt ./experiments/configurations/debug.json >outputs/syn3.json
-gosdt syn5.txt ./experiments/configurations/debug.json >outputs/syn5.json
-#gosdt tic-tac-toe.csv ./experiments/configurations/debug.json >outputs/tic-tac-toe.json
+./gosdt m2.csv experiments/configurations/debug.json >outputs/m2.json
+./gosdt experiments/datasets/monk_3/m3.csv ./experiments/configurations/debug.json >outputs/m3.json
+./gosdt syn3.txt experiments/configurations/debug.json >outputs/syn3.json
+./gosdt syn5.txt experiments/configurations/debug.json >outputs/syn5.json
+#gosdt tic-tac-toe.csv experiments/configurations/debug.json >outputs/tic-tac-toe.json
